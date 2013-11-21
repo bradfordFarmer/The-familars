b98bf149f4336db3aa8c11245ca84d3309a97bfb
bradfordFarmer <brad@propeller-com.com>
2013-11-21 16:57:34 -0500

    updating files

---
+++ gameController.js
@@ -0,0 +1,29 @@
+
+function handCtrl ($scope){
+    
+    $scope.resources=[
+        {
+        name : 'resource1' , 
+        quantity: 2,
+        }
+    ]
+    $scope.cards = [ 
+        {
+            name: "card 1"
+        }
+        
+    ];
+    
+    $scope.slots = [
+        { name: "slot 2"} ,
+         { name: "slot 1"} 
+            
+            
+    ]
+            
+    $scope.playCard = function(){
+            
+            
+    }
+        
+}
